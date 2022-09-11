2565b3a309a4082986a6157bd6afae733a1c5dbf
kochenov <kochenof@yandex.ru>
2022-09-11 13:12:30 +0300

    Add first test urls and 404 link

--- seeding/views.py
+++ seeding/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Тестовое представление')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
